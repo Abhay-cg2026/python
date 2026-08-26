5ac406298105c67d74969b2873197c7228e41e5e
Abhay Singh <abhay.m.singh.cg@gmail.com>
2026-08-26 10:09:12 +0530

    data types and arithmetic assignment added

---
+++ 04.Arithmetic operation/arithmetic.py
@@ -0,0 +1,154 @@
+#arithmetic operations basic
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+#atithmetic operation between int and float
+a=10
+b=5.0
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+print(type(a+b),type(a-b),type(a*b),type(a/b),type(a//b),type(a%b),type(a**b))
+#creating a total marks and avg marks code
+a=97
+b=98
+c=100
+print(a+b+c)
+print(a+b+c/3)
+#product calculation
+price=10.0
+quantity=10
+print(price*quantity)
+#even and odd
+a=10
+if a%2==0:
+    print("it is even")
+else:
+    print("it is odd")
+#testing division and floor division
+a=12
+b=5
+print(a/b)
+print(a//b)    
+a=-12
+b=5
+print(a/b)
+print(a//b)
+#doing operation with negative numbers
+a=-10
+b=-5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+#subtraction edge cases
+a=10
+b=5
+c=-10
+d=-5
+print(a-b)
+print(a-c)
+print(c-b)
+print(c-d)
+#floor division edge cases
+a=10
+b=5
+c=-10
+d=-5
+print(a//b)
+print(c//b)
+print(a//d)
+print(c//d)
+#modulas edge case
+a=10
+b=5
+c=-10
+d=-5
+print(a%b)
+print(c%b)
+print(a%d)
+print(c%d)
+#solving expression using python
+print(10 + 5 * 2)
+print(20 - 4 / 2)
+print(10 + 20 / 5 * 2)
+print(2 + 3 * 4 ** 2)
+print(100 - 20 // 5)
+#parenthesis
+#1
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+#2
+print(20 - 10 / 2)
+print((20 - 10) / 2)
+#3
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+#Boolean Arithmetic
+a=True
+b=False
+print(a+b)
+print(a-b)
+print(a*b)
+#boolean logic
+print(True + 5)
+print(False + 5)
+print(True * 10)
+print(False * 10)
+print(True - 5)
+print(False - 5)
+#string operation
+#task 15
+a="Abhay"
+b="singh"
+print(a+b)
+#task16
+a="abhay"
+print(a*3)
+# Task 17
+print("Task 17")
+s1 = "abc"
+s2 = "def"
+print(s1 + s2)
+print(s1 * 2)
+
+# Task 20
+print("Task 20")
+num1 = 20
+num2 = 4
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+
+# Task 21
+print("Task 21")
+a = 10
+b = -3
+c = 2.5
+
+print(a + b)
+print(a - c)
+print(a * b)
+print(a / c)
+print(a // b)
+print(a % b)
+print(b ** 2)
+print((a + b) * c)
+print(a + b * c)
+print((a - b) / c + a ** 2)
