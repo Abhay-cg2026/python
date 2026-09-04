351a8960898437ab4e3c8902134cc42261e59b90
Abhay Singh <abhay.m.singh.cg@gmail.com>
2026-09-04 11:26:06 +0530

    boolean assignment

--- 06. boolean/boolean.py
+++ 06. boolean/boolean.py
@@ -1,7 +1,17 @@
-print(bool("" and None))
-print(bool(True and True))
-print(bool(not "" and 1 or 0))
-print(bool(not 1))
-print(bool(not 0))
-print(bool(not 0 and 1 or None and ""))
-print
+#task 25
+a=int(input("Enter a number: "))
+print(a>10 and a<50)
+#task 26
+a=int(input("Enter a number: "))
+print(a<10 or a>100)
+#task 27
+print(not(6<5 and 5!=10))
+#task 29
+print(bool(0))
+print(bool(10))
+print(bool(""))
+print(bool("hello"))
+print(bool(None))
+#task 29
+
+
