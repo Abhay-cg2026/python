96fefc2f1a0dc1584ea9de57023d4269f1fcbd12
Abhay Singh <abhay.m.singh.cg@gmail.com>
2026-09-07 17:25:28 +0530

    conditional statements updated

--- 08.conditional statements/conditional statement.py
+++ 08.conditional statements/conditional statement.py
@@ -14,6 +14,8 @@ if a>0:
 marks=int(input("enter a number :"))
 if marks>=40:
     print("pass")
+else:
+    print("fail")
 #task 5
 a=int(input("Enter a number : "))
 if a==0:
@@ -50,3 +52,138 @@ if a>b:
 else:
     print(f"{b} is greater")
 #task 11
+marks=int(input("Enter a number : "))
+if marks>=90:
+    print("Grade A")
+elif marks>75 and marks<89:
+    print("Grade B")
+elif marks>60 and marks<74:
+    print("Grade C")
+elif marks>40 and marks<59:
+    print("Grade D")
+else:
+    print("fail")
+
+#task 12
+number=int(input("Enter a  number : "))
+if number>0:
+    print("Positive")
+elif number<0:
+    print("Negative")
+else:
+    print("Zero")
+
+#task 13
+day=int(input("Enter a day number : "))
+if day==1:
+    print("Monday")
+elif day==2:
+    print("Tuesday")
+elif day==3:
+    print("Wednesday")
+elif day==4:
+    print("Thursday")
+else:
+    print("Friday")
+
+#task 14
+marks=int(input("Enter a number : "))
+if marks>=90 and marks<100:
+    print("Excellent")
+elif marks>75 and marks<89:
+    print("Good")
+elif marks>40 and marks<74:
+    print("Pass")
+else:
+    print("fail")
+
+#task 15
+a=int(input("Enter a digit : "))
+if a==1:
+    print(1)
+elif a==2:
+    print(2)
+elif a==3:
+    print(3)
+else:
+    print("other")
+#task 16 
+age=int(input("Enter age : "))
+if age>=18:
+    if age<=60:
+        print("age is between 18 and 60")
+else:
+    print("age is not between 18 and 60")
+
+#task 17
+marks=int(input("Enter a number : "))
+if marks>=75:
+    print("good")
+elif marks>=40:
+    print("passed")
+else:
+    print("failed")
+
+#task 18
+a=int(input("Enter a number : "))
+if a>0:
+    if a>100:
+        print("greater than 100")
+    else:
+        print("not greater than 100")
+else:
+    input("number is negative")
+
+#task 19
+age=int(input("Enter age"))
+if age>=18:
+    print("atleast 18 yrs old")
+    if age>=60:
+        print("greater than 60")
+    else:
+        print("not greater than 60")
+else:
+    print("not greater than 18")
+
+#task 20
+n1=int(input("Enter a number : "))
+if n1!=0:
+    print("it is non zero")
+    if n1>0:
+        print("positive")
+    else:
+        print("negative") 
+else:
+    print("it is zero")
+
+#task 21
+age=int(input("Enter age : "))
+marks=int(input("Enter marks : "))
+if age>=18 and marks>=40:
+    print("Eligible")
+else:
+    print("not eligible")
+
+#task 22
+number=int(input("Enter number : "))
+if number<10 or number>100:
+    print("special")
+else:
+    print("not special")
+
+#task 23
+age=int(input("Enter age : "))
+has_id=True
+if age>=18 and has_id:
+    print("allowed")
+else:
+    print("not allowed")
+
+
+#task 24
+
+
+
+
+
+
---
+++ 08.conditional statements/practice.py
@@ -0,0 +1,8 @@
+age=int(input("Enter age : "))
+has_id=True
+if age>=18 and has_id:
+    print("allowed")
+else:
+    print("not allowed")
+
+
